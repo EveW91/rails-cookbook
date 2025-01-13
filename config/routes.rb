f015342174cafd44c4b3a70522a13aef5c149d7d
Eve Waller <evew1991@outlook.com>
2025-01-13 12:24:33 +0000

    added navbar and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root "categories#index"
+
   resources :categories, only: [:index, :create, :new, :show, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
-
+  resources :bookmarks, only: [:destroy]
 end
 
 # resources :lists, only: [:index, :show, :new, :create] do
