0dfbb2631955e1b80e51a39284ff376c55ae988d
Eve Waller <evew1991@outlook.com>
2025-01-13 10:39:05 +0000

    added bookmarks routes@

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories, only: [:index, :create, :new, :show, :destroy]
+  resources :categories, only: [:index, :create, :new, :show, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
 end
+
+# resources :lists, only: [:index, :show, :new, :create] do
+#   resources :bookmarks, only: [:new, :create]
+# end
